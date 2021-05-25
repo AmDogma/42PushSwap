8d5d2239fd024d4417f271ecede10e5822495297
mdewayne <sawden@mail.ru>
2021-05-25 14:58:39 +0300

    Initial Commit

---
+++ push_swap/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  push_swap
+//
+//  Created by Malena Dewayne on 5/25/21.
+//  Copyright © 2021 Malena Dewayne. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
